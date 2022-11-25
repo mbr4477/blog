14b453b133455da6f15c686c7f85c115f9ced847
Matthew Russell <mbr4477@gmail.com>
2022-11-25 17:29:05 -0500

    Add share buttons; add single post view

--- src/pages/microblog.js
+++ src/pages/microblog.js
@@ -1,12 +1,16 @@
-import { graphql } from "gatsby"
+import { useLocation } from "@reach/router"
+import { graphql, Link } from "gatsby"
 import moment from "moment"
+import queryString from "query-string"
 import React, { useEffect, useState } from "react"
 import ReactMarkdown from "react-markdown"
 import { GithubMicroblog } from "../api/microblog"
 import Layout from "../components/layout"
 import "./microblog.css"
 import "./spinner.css"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { faLink } from "@fortawesome/free-solid-svg-icons"
 export const pageQuery = graphql`
   query {
     site {
@@ -37,14 +41,28 @@ function Micropost(props) {
     const formattedDate = (date.isSame(moment(), "day")) ?
         date.fromNow() : `${date.format("h:mm A")} · ${date.format("MMM D, YYYY")}`
     return <article className="micropost">
+        <a id={props.isoDate} />
         <ReactMarkdown children={props.children} />
-        <div className="date">{formattedDate}</div>
+        <div className="footer">
+            <Link
+                className="micropost-share-link"
+                to={`/microblog?id=${props.isoDate}`}><FontAwesomeIcon icon={faLink} /></Link>
+            <a
+                className="micropost-share-link"
+                target="_blank"
+                rel="noreferrer"
+                href={`https://twitter.com/intent/tweet?text="${props.children}"&url=https://mruss.dev/microblog?id=${props.isoDate}`}
+            ><FontAwesomeIcon icon={faTwitter} /></a>
+            <div className="spacer"></div>
+            <div className="date">{formattedDate}</div>
+        </div>
     </article>
 }
 
 function Microblog(props) {
     const microblog = new GithubMicroblog("mbr4477", "microblog")
     const [posts, setPosts] = useState(null)
+    let postId = null
 
     useEffect(() => {
         microblog
@@ -64,9 +82,17 @@ function Microblog(props) {
                     }
                     return 1
                 })
+                console.log(posts)
                 setPosts(posts)
             })
     }, [])
+
+    const location = useLocation()
+    if (location.search) {
+        const params = queryString.parse(location.search)
+        postId = params["id"]
+        console.log(postId)
+    }
 
     const { data } = props
     const siteTitle = data.site.siteMetadata.title
@@ -75,14 +101,22 @@ function Microblog(props) {
         location={props.location}
         title={siteTitle}
         subtitle={subtitle}>
-        <h1 className="microblog-title">@mrussdev</h1>
+        {
+            postId ? <Link
+                to="/microblog"
+                className="microblog-font"
+                style={{ display: `block`, marginBottom: `10px` }}>&larr; @mrussdev</Link>
+                : <h1 className="microblog-title">@mrussdev</h1>
+        }
         {
             posts ?
-                posts.map(p => <Micropost
-                    key={p.date}
-                    isoDate={p.date}>
-                    {p.content}
-                </Micropost>)
+                posts
+                    .filter(p => postId ? (p.date === postId) : true)
+                    .map(p => <Micropost
+                        key={p.date}
+                        isoDate={p.date}>
+                        {p.content}
+                    </Micropost>)
                 : <Spinner />
         }
     </Layout>
